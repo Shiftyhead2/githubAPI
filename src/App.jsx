e84bf280f56c9548cc9074350a0a4d7a7cde675a
Shiftyhead2 <shiftyhead2@gmail.com>
2022-08-14 16:42:57 +0200

    Dodao sam komentare

--- src/App.jsx
+++ src/App.jsx
@@ -7,13 +7,17 @@ function App() {
   const [user,setUser] = useState(null);
   const [searchText,setSearchText] = useState("");
 
+
   useEffect(() => {
+    /* fetchaj korisnika s githuba samo ako searchText(username) nije prazan string  */
     if(searchText.length > 0){
       fetch(`https://api.github.com/users/${searchText}`)
       .then((res) => {
+        /* Ovo je blok koji provjerava da li nam response vraća kod 200. Ako ne onda imamo error koji moramo ispisati korisniku. */
         if(res.status != 200){
           throw Error(`Responded with code ${res.status}: User ${res.statusText.toLocaleLowerCase()}`);
         }else{
+          /* Ovo je i mene zezalo dok nisam shvatio što sam zajebo. Kada radimo sa API-om uvijek moramo returnati(vratiti) što god smo dobili u responsu u json formatu*/
           return res.json();
         }
       })
@@ -25,6 +29,7 @@ function App() {
 
   return (
     <div className="flex justify-center">
+      {/* Ako user postoji(nije null) prikaži User komponentu. Ako user ne postoji(null je) prikaži SearchForm komponentu */}
       {user ? <User user = {user} setUser = {setUser} /> : <SearchForm setSearchText = {setSearchText} />}
     </div>
   );
--- src/components/User.jsx
+++ src/components/User.jsx
@@ -6,9 +6,11 @@ const User = ({user,setUser}) => {
   const [repos,setRepos] = useState(null);
 
   useEffect(() => {
+    /* Fetchaj repos od usera samo ako user postoji(nije null) */
     if(user){
       fetch(`https://api.github.com/users/${user.login}/repos`)
       .then((res) => {
+        /* Ovo je ista provjera koju sam napravio u App.jsx komponenti. OK je kod 200 */
         if(!res.ok){
           throw Error(`Responded with code ${res.status}: ${res.statusText}`);
         }else{
@@ -27,6 +29,7 @@ const User = ({user,setUser}) => {
       <h2 className="text-xl">Name:{user.name}</h2>
       <h2 className="text-xl break-words mx-60">Bio:{user.bio}</h2>
       <h2 className='text-xl'>Repos:</h2>
+      {/* Ako reposi postoje(nisu null) onda prikaži ReposList komponentu. Ako reposi ne postoje(null su) prikaži paragraph koji kaže da nije mogao pronaći repose */}
       {repos ? <ReposList repos = {repos} /> : <p>No repos found!</p>}
       <button className="bg-lime-500  w-1/5 text-gray-800 py-2 px-3 rounded hover:bg-lime-400 hover:text-gray-900" onClick={(e) => { e.preventDefault()
         setUser(null)
